359f9887dc04eef638f8fa08133f382ef6aa140f
Ulrich Fladl <ulricof@gmail.com>
2022-04-11 15:30:47 +0200

    end of day commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nisi reiciendis sint, cumque molestias est ut amet quas alias ad officiis enim explicabo aspernatur consequuntur, mollitia accusantium optio. Hic, blanditiis?</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quibusdam aliquid numquam. Nam, nulla laboriosam eius quis adipisci voluptatem perferendis, officia iste dolore id minus, neque beatae totam magni. Ad.</p>  
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="dispaly-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
